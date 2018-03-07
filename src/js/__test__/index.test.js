ecc74607032de1024afd71c04b85ac2da6b01b3b
Chris Boakes <chris@chrisboakes.com>
2018-03-07 12:49:31 +0000

    Updated test

--- src/js/__test__/index.test.js
+++ src/js/__test__/index.test.js
@@ -9,10 +9,17 @@ describe('Options should combine', () => {
         });
 
         let output = {
+            policyWording: 'We use cookies to create your experience of our website better. To comply with the E-Privacy Regulations, we need to ask for your consent to set these cookies.',
             anchorBottom: false,
-            backgroundColour: '#000',
-            textColour: '#FFF',
-            policyWording: 'We use cookies to create your experience of our website better. To comply with the E-Privacy Regulations, we need to ask for your consent to set these cookies.'
+            barColor: '#333',
+            buttonColor: '#CCC',
+            barTextColor: '#FFF',
+            buttonTextColor: '#333',
+            barClassName: 'c-cookie-bar',
+            fontFamily: 'Arial',
+            fontSize: '16px',
+            barVerticalPadding: '20px',
+            findOutMoreLink: ''
         };
 
         expect(input).toEqual(output);
