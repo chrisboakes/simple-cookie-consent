09b395f9b610419ad509e577b3de251ee9f84526
Chris Boakes <chris@chrisboakes.com>
2018-03-12 11:56:26 +0000

    Updated tests

--- src/js/__test__/dom-elements.test.js
+++ src/js/__test__/dom-elements.test.js
@@ -8,7 +8,7 @@ describe('Cookie Bar Markup', () => {
             barClassName: 'c-cookie-bar'
         });
 
-        let output = `<div class=\"c-cookie-bar\"><div class=\"c-cookie-bar__inner\"><p class=\"c-cookie-bar__policy\">We use cookies to create your experience of our website better. To comply with the E-Privacy Regulations, we need to ask for your consent to set these cookies.</p><div class=\"c-cookie-bar__buttons-wrap\"><button class=\"c-cookie-bar__button\" aria-label=\"Accept consent to set cookies\"><span class=\"c-cookie-bar__button-text\">Accept <span class=\"c-cookie-bar__accessible-text\">consent to set cookies</span></span></button></div></div></div>`;
+        let output = `<div class=\"c-cookie-bar\"><div class=\"c-cookie-bar__inner\"><p class=\"c-cookie-bar__policy\">We use cookies to create your experience of our website better. To comply with the E-Privacy Regulations, we need to ask for your consent to set these cookies.</p><div class=\"c-cookie-bar__buttons-wrap\"><button class=\"c-cookie-bar__button c-cookie-bar__button--accept\" aria-label=\"Accept consent to set cookies\"><span class=\"c-cookie-bar__button-text\">Accept <span class=\"c-cookie-bar__accessible-text\">consent to set cookies</span></span></button></div></div></div>`;
 
         expect(input).toBe(output);
     });
@@ -21,7 +21,7 @@ describe('Cookie Bar Markup', () => {
             additionalLinkText: 'Find out more'
         });
 
-        let output = `<div class=\"c-cookie-bar c-cookie-bar--has-additional\"><div class=\"c-cookie-bar__inner\"><p class=\"c-cookie-bar__policy\">We use cookies to create your experience of our website better. To comply with the E-Privacy Regulations, we need to ask for your consent to set these cookies.</p><div class=\"c-cookie-bar__buttons-wrap\"><button class=\"c-cookie-bar__button\" aria-label=\"Accept consent to set cookies\"><span class=\"c-cookie-bar__button-text\">Accept <span class=\"c-cookie-bar__accessible-text\">consent to set cookies</span></span></button><a class=\"c-cookie-bar__button c-cookie-bar__button--additional\" href=\"#\"><span class=\"c-cookie-bar__button-text\">Find out more</span></a></div></div></div>`;
+        let output = `<div class=\"c-cookie-bar c-cookie-bar--has-additional\"><div class=\"c-cookie-bar__inner\"><p class=\"c-cookie-bar__policy\">We use cookies to create your experience of our website better. To comply with the E-Privacy Regulations, we need to ask for your consent to set these cookies.</p><div class=\"c-cookie-bar__buttons-wrap\"><button class=\"c-cookie-bar__button c-cookie-bar__button--accept\" aria-label=\"Accept consent to set cookies\"><span class=\"c-cookie-bar__button-text\">Accept <span class=\"c-cookie-bar__accessible-text\">consent to set cookies</span></span></button><a class=\"c-cookie-bar__button c-cookie-bar__button--additional\" href=\"#\"><span class=\"c-cookie-bar__button-text\">Find out more</span></a></div></div></div>`;
 
         expect(input).toBe(output);
     });
@@ -32,7 +32,7 @@ describe('Cookie Bar Markup', () => {
             barClassName: 'custom-class-name'
         });
 
-        let output = `<div class=\"custom-class-name\"><div class=\"custom-class-name__inner\"><p class=\"custom-class-name__policy\">We use cookies to create your experience of our website better. To comply with the E-Privacy Regulations, we need to ask for your consent to set these cookies.</p><div class=\"custom-class-name__buttons-wrap\"><button class=\"custom-class-name__button\" aria-label=\"Accept consent to set cookies\"><span class=\"custom-class-name__button-text\">Accept <span class=\"custom-class-name__accessible-text\">consent to set cookies</span></span></button></div></div></div>`;
+        let output = `<div class=\"custom-class-name\"><div class=\"custom-class-name__inner\"><p class=\"custom-class-name__policy\">We use cookies to create your experience of our website better. To comply with the E-Privacy Regulations, we need to ask for your consent to set these cookies.</p><div class=\"custom-class-name__buttons-wrap\"><button class=\"custom-class-name__button custom-class-name__button--accept\" aria-label=\"Accept consent to set cookies\"><span class=\"custom-class-name__button-text\">Accept <span class=\"custom-class-name__accessible-text\">consent to set cookies</span></span></button></div></div></div>`;
 
         expect(input).toBe(output);
     });
